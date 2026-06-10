3c2277a1e712c5df6d4e3f60c1427ccd50f2bc1e
Zamir <zamir@example.com>
2026-06-11 00:44:30 +0530

    Fix Arrow_White emoji to be static

--- backend/src/bot/commands/listgame.js
+++ backend/src/bot/commands/listgame.js
@@ -32,7 +32,7 @@ export default {
 
       let description = '';
       for (const game of availableGames) {
-        description += `<a:Arrow_White:1400099341578014793> **${game._id}** (${game.count} available)\n`;
+        description += `<:Arrow_White:1400099341578014793> **${game._id}** (${game.count} available)\n`;
       }
 
       embed.setDescription(description);
